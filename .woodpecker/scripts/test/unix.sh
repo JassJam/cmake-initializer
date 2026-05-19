3d384b389a3cfba0caa6716f483bad1bf393d678
JassJam <0139ne@gmail.com>
2026-05-19 11:55:42 +0200

    Dont use sudo

--- .woodpecker/scripts/test/unix.sh
+++ .woodpecker/scripts/test/unix.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 echo "=== Installing system dependencies (Linux) ==="
-sudo apt-get update -q
-sudo apt-get install -y build-essential ninja-build cmake pwsh
+apt-get update -q
+apt-get install -y build-essential ninja-build cmake pwsh
 
 # Install Clang if the preset requires it
 if [[ "$CONFIG_PRESET" == *clang* ]]; then
     echo "=== Installing Clang ==="
-    sudo apt-get install -y clang
+    apt-get install -y clang
 fi
 
 echo "=== Running common test logic ==="
