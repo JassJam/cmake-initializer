5ae22acc45af59f11ea6e54afbb0b1d4bb35d374
JassJam2 <0139ne@gmail.com>
2026-05-20 22:32:47 +0100

    Add build phase

--- .woodpecker/scripts/test/setup-unix.sh
+++ .woodpecker/scripts/test/setup-unix.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
 echo "=== Installing system dependencies (Linux) ==="
 apt-get update -q
 apt-get install -y build-essential ninja-build cmake curl git libicu-dev libssl-dev
