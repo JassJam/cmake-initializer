3aa9e80391f87f49c50316f510e0a3f97b840eb2
JassJam <0139ne@gmail.com>
2026-05-21 14:27:59 +0200

    use emscripten/emsdk:latest directly

--- .woodpecker/scripts/test/setup-emscripten.sh
+++ .woodpecker/scripts/test/setup-emscripten.sh
@@ -6,15 +6,14 @@ apt-get update -q
 apt-get install -y build-essential ninja-build cmake curl git libicu-dev libssl-dev python3
 
 # Install xmake/xrepo
-# _saved_EMSDK="${EMSDK:-}"
-# unset EMSDK
+_saved_EMSDK="${EMSDK:-}"
+unset EMSDK
 
 curl -fsSL https://xmake.io/shget.text | bash
 export PATH="$HOME/.local/bin:$PATH"
-export XMAKE_ROOT=y
 xmake update -v dev
 
-# export EMSDK="$_saved_EMSDK"
+export EMSDK="$_saved_EMSDK"
 
 # Verify xrepo is working
 xrepo --version
