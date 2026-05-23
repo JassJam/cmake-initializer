906b98ab0fb3b44c581997ec459bee99a7d3f473
JassJam2 <0139ne@gmail.com>
2026-05-23 19:31:10 +0000

    Update emscripten version

--- samples/hello_emscripten/src/main.cpp
+++ samples/hello_emscripten/src/main.cpp
@@ -21,6 +21,30 @@ int EMSCRIPTEN_KEEPALIVE add_numbers(int a, int b)
 }
 }
 
+#if defined(__EMSCRIPTEN_MAJOR__)
+#define VERSION_EMSCRIPTEN_MAJOR __EMSCRIPTEN_MAJOR__
+#elif defined(__EMSCRIPTEN_major__)
+#define VERSION_EMSCRIPTEN_MAJOR __EMSCRIPTEN_major__
+#else
+#define VERSION_EMSCRIPTEN_MAJOR "x"
+#endif
+
+#if defined(__EMSCRIPTEN_MINOR__)
+#define VERSION_EMSCRIPTEN_MINOR __EMSCRIPTEN_MINOR__
+#elif defined(__EMSCRIPTEN_minor__)
+#define VERSION_EMSCRIPTEN_MINOR __EMSCRIPTEN_minor__
+#else
+#define VERSION_EMSCRIPTEN_MINOR "x"
+#endif
+
+#if defined(__EMSCRIPTEN_TINY__)
+#define VERSION_EMSCRIPTEN_TINY __EMSCRIPTEN_TINY__
+#elif defined(__EMSCRIPTEN_tiny__)
+#define VERSION_EMSCRIPTEN_TINY __EMSCRIPTEN_tiny__
+#else
+#define VERSION_EMSCRIPTEN_TINY "x"
+#endif
+
 #endif
 
 int main()
@@ -28,9 +52,10 @@ int main()
     std::cout << "=== Hello Emscripten Example ===" << std::endl;
 
 #ifdef __EMSCRIPTEN__
+
     std::cout << "Running in WebAssembly environment!" << std::endl;
-    std::cout << "Emscripten version: " << __EMSCRIPTEN_major__ << "." << __EMSCRIPTEN_minor__
-              << "." << __EMSCRIPTEN_tiny__ << std::endl;
+    std::cout << "Emscripten version: " << VERSION_EMSCRIPTEN_MAJOR << "." << VERSION_EMSCRIPTEN_MINOR
+              << "." << VERSION_EMSCRIPTEN_TINY << std::endl;
 
     // Print some information about the environment
     std::cout << "This example demonstrates:" << std::endl;
